b9d40b656ba39f592531f226f5002f1f0449f4fb
MindistCalm <suxinin5@yandex.ru>
2025-03-28 22:19:34 +0700

    2 | added receipt new form, added navigation back into HeaderGeneral, added BasicBlock

--- app/src/components/BasicBlock/index.tsx
+++ app/src/components/BasicBlock/index.tsx
@@ -3,8 +3,22 @@ import { ReactNode } from 'react'
 interface BasicBlockProps {
   className?: string
   children?: ReactNode | string
+  onClick?: VoidFunction
 }
 
-export const BasicBlock = ({ className = '', children }: BasicBlockProps) => {
-  return <div className={`p-4 shadow-md rounded-2xl ${className}`}>{children}</div>
+export const BasicBlock = ({
+  className = '',
+  children,
+  onClick = () => {},
+  ...rest
+}: BasicBlockProps) => {
+  return (
+    <div
+      className={`rounded-2xl p-4 shadow-md ${className}`}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </div>
+  )
 }
