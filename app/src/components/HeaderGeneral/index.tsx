f13a67a40c4c78efc2902a74fd2bd84da2b44c06
MindistCalm <suxinin5@yandex.ru>
2025-03-28 22:21:47 +0700

    inter commit

--- app/src/components/HeaderGeneral/index.tsx
+++ app/src/components/HeaderGeneral/index.tsx
@@ -1,13 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 import { ChevronRight24 } from '../../assets/icons'
 
 interface HeaderGeneralProps {
   title?: string
+  onClick: VoidFunction
 }
 
 export const HeaderGeneral = ({ title }: HeaderGeneralProps) => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate(-1)
+  }
+
   return (
     <div className="relative flex h-[68px] max-h-17 items-center px-4 py-3 shadow-md">
-      <button className="absolute h-11 cursor-pointer">
+      <button className="absolute h-11 cursor-pointer" onClick={handleClick}>
         <img
           src={ChevronRight24}
           className="rotate-180 stroke-blue-400"
